847daf532b8b806e19798f5d3360a08336000d77
Conrad Etherington <conrad.etherington@gmail.com>
2022-11-28 10:05:37 -0700

    commit tree a89201d08076e374320b7316f305743c0ff92c67

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,36 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence? 
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+self.end_with?("!")
   end
 
   def count_sentences
-
-  end
+    self.split(/[?.!]/).reject{|s|s.empty?}.count
+    # splitselfplus = self.split("!")
+    # splitselfplusplus = self.split("?")
+    # arraymagic = []
+    # arraymagic << splitself.count
+    # arraymagic << splitselfplus.count
+    # arraymagic << splitselfplusplus.count
+    # binding.pry
+    # counter = arraymagic.count > 0 ? arraymagic.count - 2 : 0
+    # counter
+    # spell failed
+  #   counter= 0
+  #   withoutEmptyStr = splitself.select do |string|
+  #     string != ""
+  #   end
+  #   counter = withoutEmptyStr.count
+  #  binding.pry
+  #   counter
+   end
 end
